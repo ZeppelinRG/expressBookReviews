4b7b26006bf01fc17213346551956b45144227b9
arya <joshiarya08@gmail.com>
2024-08-20 15:38:36 -0400

    finished project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
@@ -25,6 +26,18 @@ public_users.get('/',function (req, res) {
   return res.send(books);
 });
 
+//task 10
+public_users.get('/async', async (req, res) => {
+    try{
+        const response = await axios.get(
+            "https://joshiarya08-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/");
+        return res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        return res.status(410).json({ message: "Error retrieving book list" });
+      }
+  });
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
@@ -33,6 +46,21 @@ public_users.get('/isbn/:isbn',function (req, res) {
     }
     return res.send("ISBN not found!");
  });
+
+//task 11
+public_users.get('/async/isbn/:isbn', async (req, res) => {
+    try {
+        const isbn = req.params.isbn;
+        const response = 
+        await axios.get(
+            "https://joshiarya08-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/"
+             + isbn);
+        return res.json(response.data);
+    } catch {
+        console.error(error);
+        return res.status(410).json({ message: "Error retrieving book with requested ISBN." });
+    }
+    });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -48,6 +76,21 @@ public_users.get('/author/:author',function (req, res) {
     return res.send(final);
 });
 
+//task 12
+public_users.get('/async/author/:author', async (req, res) => {
+    try {
+        const author = req.params.author;
+        const response = 
+        await axios.get(
+        "https://joshiarya08-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/"
+            + author);
+        return res.json(response.data);
+    } catch {
+        console.error(error);
+        return res.status(410).json({ message: "Error retrieving book with requested author." });
+    }
+    });
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     let isbn = Object.keys(books);
@@ -61,6 +104,21 @@ public_users.get('/title/:title',function (req, res) {
     const final = {"booksbytitle": existingTitle};
     return res.send(final);
 });
+
+//task 13
+public_users.get('/async/title/:title', async (req, res) => {
+    try {
+        const title = req.params.title;
+        const response = 
+        await axios.get(
+        "https://joshiarya08-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/"
+            + title);
+        return res.json(response.data);
+    } catch {
+        console.error(error);
+        return res.status(410).json({ message: "Error retrieving book with requested title." });
+    }
+    });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
